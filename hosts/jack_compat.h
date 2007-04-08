274936c394d45abeaba05fc0c5bd1e19250492e2
David Robillard <d@drobilla.net>
2007-04-08 18:02:39 +0000

    Yet.  More.  Jack.  MIDI.  API.  Hacks.
    git-svn-id: http://svn.drobilla.net/lad/slv2@422 a436a847-0d15-0410-975c-d299462d15a1

--- hosts/jack_compat.h
+++ hosts/jack_compat.h
@@ -18,20 +18,23 @@
 #ifndef JACK_COMPAT_H
 #define JACK_COMPAT_H
 
-#if defined(HAVE_OLD_JACK_MIDI)
-#define jack_midi_get_event_count(port_buf, nframes) jack_midi_port_get_info(port_buf, nframes)->event_count
-#endif
 
 #if defined(JACK_MIDI_NEEDS_NFRAMES)
 
-inline jack_nframes_t
+jack_nframes_t
 jack_midi_get_event_count_compat(
   void * port_buffer)
 {
+#if defined(HAVE_OLD_JACK_MIDI)
+  return jack_midi_port_get_info(port_buffer, 0)->event_count;
+#else
   return jack_midi_get_event_count(port_buffer, 0);
+#endif
 }
 
-inline int
+#define jack_midi_get_event_count jack_midi_get_event_count_compat
+
+int
 jack_midi_event_get_compat(
   jack_midi_event_t * event,
   void * port_buffer,
@@ -40,9 +43,14 @@ jack_midi_event_get_compat(
   return jack_midi_event_get(event, port_buffer, event_index, 0);
 }
 
-#define jack_midi_get_event_count jack_midi_get_event_count_compat
 #define jack_midi_event_get jack_midi_event_get_compat
 
-#endif  /* #if defined(HAVE_JACK_MIDI_WITH_NFRAMES) */
+#else
+
+#if defined(HAVE_OLD_JACK_MIDI)
+#error "Old (0.102.20) JACK MIDI API needs nframes (autotools probably gone mad)"
+#endif
+
+#endif  /* #if defined(JACK_MIDI_NEEDS_NFRAMES) */
 
 #endif /* JACK_COMPAT_H */
