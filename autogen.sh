b83ce0b6712e9dc119dc6fa38c58932c60321ed1
David Robillard <d@drobilla.net>
2008-01-16 02:27:40 +0000

    Mac building fixes.
    git-svn-id: http://svn.drobilla.net/lad/slv2@1062 a436a847-0d15-0410-975c-d299462d15a1

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,14 @@
 echo 'Generating necessary files...'
 rm -rf config
 mkdir -p config
-libtoolize --force
+# Mac
+if [ -x "`which glibtoolize`" ]; then
+	glibtoolize --force
+else
+	libtoolize --force
+fi
 aclocal
 autoheader -Wall
-automake --foreign --add-missing -Wall -Wno-portability
+automake --foreign --add-missing
 autoconf
 
