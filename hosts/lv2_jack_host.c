108c0af7f15e1aeca657546518bed8180cd47ef6
David Robillard <d@drobilla.net>
2008-12-21 17:04:40 +0000

    Change configuration header name to something less likely to conflict.
    git-svn-id: http://svn.drobilla.net/lad/trunk/slv2@1887 a436a847-0d15-0410-975c-d299462d15a1

--- hosts/lv2_jack_host.c
+++ hosts/lv2_jack_host.c
@@ -16,7 +16,7 @@
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "config.h"
+#include "wafconfig.h"
 
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
--- utils/lv2_list.c
+++ utils/lv2_list.c
@@ -16,7 +16,7 @@
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "config.h"
+#include "wafconfig.h"
 #include <stdio.h>
 #include <string.h>
 #include "slv2/slv2.h"
