lv2/lilv

commit 385be0465af010722fa3a99890d30674be3d2d0c

Use rdfs:label for preset labels (fix ticket #668). Use correct namespace for dc.
git-svn-id: http://svn.drobilla.net/lad/trunk/lilv@3593 a436a847-0d15-0410-975c-d299462d15a1

utils/lv2info.c
@@ -29,7 +29,7 @@ LilvNode* control_class       = NULL;
 LilvNode* in_group_pred       = NULL;
 LilvNode* role_pred           = NULL;
 LilvNode* preset_pred         = NULL;
-LilvNode* title_pred          = NULL;
+LilvNode* label_pred          = NULL;
 LilvNode* supports_event_pred = NULL;
 
 void
@@ -360,7 +360,7 @@ main(int argc, char** argv)
 	LilvWorld* world = lilv_world_new();
 	lilv_world_load_all(world);
 
-#define NS_DC   "http://dublincore.org/documents/dcmi-namespace/"
+#define NS_DC   "http://purl.org/dc/elements/1.1/"
 #define NS_PG   "http://lv2plug.in/ns/ext/port-groups#"
 #define NS_PSET "http://lv2plug.in/ns/ext/presets#"
 #define NS_EV   "http://lv2plug.in/ns/ext/event#"
@@ -370,7 +370,7 @@ main(int argc, char** argv)
 	in_group_pred       = lilv_new_uri(world, NS_PG "inGroup");
 	preset_pred         = lilv_new_uri(world, NS_PSET "hasPreset");
 	role_pred           = lilv_new_uri(world, NS_PG "role");
-	title_pred          = lilv_new_uri(world, NS_DC "title");
+	label_pred          = lilv_new_uri(world, LILB_NS_RDFS "label");
 	supports_event_pred = lilv_new_uri(world, NS_EV "supportsEvent");
 
 	const LilvPlugins* plugins = lilv_world_get_all_plugins(world);
@@ -401,7 +401,7 @@ main(int argc, char** argv)
 
 	lilv_node_free(uri);
 
-	lilv_node_free(title_pred);
+	lilv_node_free(label_pred);
 	lilv_node_free(role_pred);
 	lilv_node_free(preset_pred);
 	lilv_node_free(in_group_pred);

src/world.c
@@ -47,7 +47,7 @@ lilv_world_new(void)
 	world->plugins        = lilv_plugins_new();
 
 #define NS_DYNMAN "http://lv2plug.in/ns/ext/dynmanifest#"
-#define NS_DC     "http://dublincore.org/documents/dcmi-namespace/"
+#define NS_DC     "http://purl.org/dc/elements/1.1/"
 
 #define NEW_URI(uri)     sord_new_uri(world->world, (const uint8_t*)uri)
 #define NEW_URI_VAL(uri) lilv_new_uri(world, (const char*)(uri));

commit b8441be18d69447fcfa8c25ffce74d0a911f4a47

Fix compilation broken in r3593.
git-svn-id: http://svn.drobilla.net/lad/trunk/lilv@3595 a436a847-0d15-0410-975c-d299462d15a1

utils/lv2info.c
@@ -274,7 +274,7 @@ print_plugin(LilvWorld*        world,
 	LILV_FOREACH(nodes, i, presets) {
 		LilvNodes* titles = lilv_world_find_nodes(world,
 		                                          lilv_nodes_get(presets, i),
-		                                          title_pred,
+		                                          label_pred,
 		                                          NULL);
 		if (titles) {
 			const LilvNode* title = lilv_nodes_get(titles, lilv_nodes_begin(titles));
@@ -369,7 +369,7 @@ main(int argc, char** argv)
 	in_group_pred       = lilv_new_uri(world, NS_PG "inGroup");
 	preset_pred         = lilv_new_uri(world, NS_PSET "hasPreset");
 	role_pred           = lilv_new_uri(world, NS_PG "role");
-	label_pred          = lilv_new_uri(world, LILB_NS_RDFS "label");
+	label_pred          = lilv_new_uri(world, LILV_NS_RDFS "label");
 	supports_event_pred = lilv_new_uri(world, NS_EV "supportsEvent");
 
 	const LilvPlugins* plugins = lilv_world_get_all_plugins(world);