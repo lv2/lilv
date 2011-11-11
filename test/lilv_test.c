d2388d3aad0f7cc591be1df5a31877cf23bdaf15
David Robillard <d@drobilla.net>
2011-11-11 19:38:41 +0000

    Add feature test macro required for setenv.
    git-svn-id: http://svn.drobilla.net/lad/trunk/lilv@3609 a436a847-0d15-0410-975c-d299462d15a1

--- test/lilv_test.c
+++ test/lilv_test.c
@@ -15,6 +15,8 @@
   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
+#define _POSIX_C_SOURCE 200112L /* for setenv */
+
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
