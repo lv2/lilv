41a1a23f81ca279a0d8baf0c85ab97db9e267f9a
David Robillard <d@drobilla.net>
2019-04-14 10:52:25 +0200

    Remove redundant declaration of LILV_DEPRECATED

--- lilv/lilvmm.hpp
+++ lilv/lilvmm.hpp
@@ -19,12 +19,6 @@
 
 #include "lilv/lilv.h"
 
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1)
-#    define LILV_DEPRECATED __attribute__((__deprecated__))
-#else
-#    define LILV_DEPRECATED
-#endif
-
 namespace Lilv {
 
 LILV_DEPRECATED
