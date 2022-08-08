e6b7962a4d3b446bd52e41e3c23fda2d464ab936
David Robillard <d@drobilla.net>
2022-08-08 12:10:17 -0400

    lv2bench: Use CLOCK_MONOTONIC

--- tools/bench.h
+++ tools/bench.h
@@ -19,7 +19,7 @@ static inline BenchmarkTime
 bench_start(void)
 {
   BenchmarkTime start_t;
-  clock_gettime(CLOCK_REALTIME, &start_t);
+  clock_gettime(CLOCK_MONOTONIC, &start_t);
   return start_t;
 }
 
@@ -27,7 +27,7 @@ static inline double
 bench_end(const BenchmarkTime* start_t)
 {
   BenchmarkTime end_t;
-  clock_gettime(CLOCK_REALTIME, &end_t);
+  clock_gettime(CLOCK_MONOTONIC, &end_t);
   return bench_elapsed_s(start_t, &end_t);
 }
 
