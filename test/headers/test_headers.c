4020bc9376eb77d8b6493d98bf9dc6c5b03e41dc
David Robillard <d@drobilla.net>
2025-01-20 13:16:57 -0500

    Use "system" include type for all dependencies and add header test
    Things get confused when these flags differ across projects, so universally
    use "system" for external dependencies and test for header warnings only in
    the project that owns them.

---
+++ test/headers/test_headers.c
@@ -0,0 +1,13 @@
+// Copyright 2022-2025 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#include <lilv/lilv.h> // IWYU pragma: keep
+
+#ifdef __GNUC__
+__attribute__((const))
+#endif
+int
+main(void)
+{
+  return 0;
+}
