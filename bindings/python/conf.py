97601cb262e0f40f7329009f6278f2343c5558b6
David Robillard <d@drobilla.net>
2025-12-20 17:58:00 -0500

    Suppress new warnings in clang and clang-tidy 21

--- bindings/python/conf.py
+++ bindings/python/conf.py
@@ -60,7 +60,7 @@ copyright = u'2016, David Robillard'
 # built documents.
 #
 # The short X.Y version.
-version = '0.26.2'
+version = '0.26.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- src/lilv_config.h
+++ src/lilv_config.h
@@ -5,7 +5,7 @@
 #define LILV_CONFIG_H
 
 // Define version unconditionally so a warning will catch a mismatch
-#define LILV_VERSION "0.26.2"
+#define LILV_VERSION "0.26.3"
 
 // Separator between entries in variables like PATH
 #ifndef LILV_PATH_SEP
